6a4eed1d6903adedadcc9d4697bc500fa2547b9f
davidba659133 <38923177+davidba659133@users.noreply.github.com>
2018-07-23 14:59:30 +0200

    datetype
    ajout du use

---
+++ src/Migrations/Version20180723124935.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180723124935 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users ADD username VARCHAR(50) NOT NULL, ADD roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', DROP pseudo, DROP role');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users ADD role VARCHAR(50) NOT NULL COLLATE utf8mb4_unicode_ci, DROP roles, CHANGE username pseudo VARCHAR(50) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
