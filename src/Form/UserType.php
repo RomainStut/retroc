c50eedcdc1af1cfb083bad922c5588749f97105b
davidba659133 <38923177+davidba659133@users.noreply.github.com>
2018-07-23 13:59:23 +0200

    formulaire
    création des UsersTypes

---
+++ src/Form/UserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo', TextType::class)
+            ->add('email', EmailType::class, array('label' => 'Ecrivez votre email'))
+            ->add('tel', TelType::class, array('label' => 'Ecrivez votre téléphone'))
+            ->add('datebirth', DateType::class)
+            ->add('address', TextType::class)
+            ->add('password',RepeatedType::class, array('type'=> PasswordType::class, 'invalid_message' => 'les mdp ne sont pas identiques',
+                'first_options' => ['label' => 'mot de passe'],
+                'second_options' => ['label' => 'repetez le mot de passe']))
+            ->add('codepostal', IntegerType::class)
+            ->add('city', TextType::class)
+            ->add('s\'inscrire', SubmitType::class, array('label'=>'s\'inscrire', 'attr' => ['class' => 'btn btn-info']));
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
