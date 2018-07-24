4f4b9c4670fda89033ad1f756d69391a60cfced6
RomainStut <39411840+RomainStut@users.noreply.github.com>
2018-07-24 11:10:38 +0200

    footer

---
+++ src/Controller/InformationsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class InformationsController extends Controller
+{
+    /**
+     * @Route("/informations", name="informations")
+     */
+    public function index()
+    {
+        return $this->render('informations/index.html.twig', [
+            'controller_name' => 'InformationsController',
+        ]);
+    }
+
+    /**
+     * @Route("/informations/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('informations/contact.html.twig');
+    }
+}
