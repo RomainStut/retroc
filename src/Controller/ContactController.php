8cf7b2d045a9ed25f7033fac7bf59a4c0333fe45
RomainStut <39411840+RomainStut@users.noreply.github.com>
2018-07-25 12:18:32 +0200

    formulaire de contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,34 +5,18 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class InformationsController extends Controller
+class ContactController extends Controller
 {
     /**
-     * @Route("/informations", name="informations")
+     * @Route("/contact", name="contact")
      */
     public function index()
     {
-        return $this->render('informations/index.html.twig', [
-            'controller_name' => 'InformationsController',
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
         ]);
     }
 
-    /**
-     * @Route("/informations/contact", name="contact")
-     */
-    public function contact()
-    {
-        return $this->render('informations/contact.html.twig');
-    }
-    /**
-     * @Route("/informations/cgv", name="cgv")
-     */
-    
-    public function cgv()
-    {
-        return $this->render('informations/cgv.html.twig');
-    }
-    
     public function contactmail(\Swift_Mailer $mailer)
    {
        $confirmMessage="";
@@ -75,3 +59,4 @@ class InformationsController extends Controller
    }
 
 }
+
