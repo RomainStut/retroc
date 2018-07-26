2dae866e220495a05835798eccbe3bdb037f1024
davidba659133 <38923177+davidba659133@users.noreply.github.com>
2018-07-26 12:27:06 +0200

    page modif user depuis profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\UpdateUserType;
 use App\Form\UserType;
 use App\Service\FileUploader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Users;
+use Symfony\Component\HttpFoundation\File\File;
+
 
 class UserController extends Controller
 {
@@ -26,48 +29,51 @@ class UserController extends Controller
     public function showUser()
     {
 
-    	$this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-    	$userId = $this->getUser();
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $userId = $this->getUser();
 
         $repository = $this->getDoctrine()->getRepository(Users::class);
-        $user = $repository->find($userId);       
+        $user = $repository->find($userId);
         // nous permet de renvoyer un message d'erreur si aucun id ne correspond
         if(!$user){
             throw $this->createNotFoundException('No user found for id '.$id);
         }
-         	return $this->render('user/infoUser.html.twig', array('user'=>$user));
+        return $this->render('user/infoUser.html.twig', array('user'=>$user));
     }
 
     /**
-     * @route("profil/modifier", name="modifUser")
+     * @route("/profil/modifier/{id}", name="modif-user", requirements= {"id"="\d+"})
      */
-    public function updateUser(Users $users, Request $request, FileUploader $uploader)
+    public function updateUser($id, Users $users, Request $request, FileUploader $uploader)
     {
-        $fileName = $users->getImage();
+//j'utilise mon voter pour déterminer si l'utilisateur peut modifier cet article
+/*        $this->denyAccessUnlessGranted('edit', $users);*/
+
+
+        /*$fileName = $users->getImage();
         if($users->getImage()) {
-//pour povoir générer le formulaire, on doit transformer le nom du fichier stocké pour l'instant
-            // dans l'attribut image en instance de la classe File (ce qui est attendu par le formulaire)
             $users->setImage(new File($this->getParameter('articles_image_directory') . '/' . $users->getImage()));
-        }
+        }*/
 
-        $form = $this->createForm(UserType::class, $users);
+        $form = $this->createForm(UpdateUserType::class, $users);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $category = $form->getData();
-            if ($users->getImage()){
+
+            $users = $form->getData();
+           /* if ($users->getImage()){
                 //on récupère un objet de classe File
                 $file = $users->getImage();
 
-                $fileName = $uploader->upload($file, $fileName);
+                $users = $uploader->upload($file, $fileName);
             }
-            $users->setImage($fileName);
+            $users->setImage($fileName);*/
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->flush();
-            $this->addFlash('success', 'Utilisateur modifiée !');
+            $this->addFlash('success', 'Utilisateur modifié !');
             return $this->redirectToRoute('userProfil');
-
         }
+
+
         return$this->render('user/modifUser.html.twig', array('form' => $form->createView()));
     }
 }
