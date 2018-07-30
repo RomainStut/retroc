a8357b2410b0fed1738f1fe83032934b6e19b380
davidba659133 <38923177+davidba659133@users.noreply.github.com>
2018-07-30 11:58:31 +0200

    gestion du blog user ou admin

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\File\File;
 class BlogController extends Controller
 {
     /**
-     * @Route("blog/show-all", name="show-all")
+     * @Route("/blog/show-all", name="show-all")
      */
     public function showAll()
     {
@@ -27,7 +27,7 @@ class BlogController extends Controller
 
 
     /**
-     * @Route("admin/blog/{id}", name="blog", requirements = {"id"="[0-9]+"})
+     * @Route("blog/article/{id}", name="article-blog", requirements = {"id"="[0-9]+"})
      */
     public function infoArticle($id)
     {
@@ -36,7 +36,7 @@ class BlogController extends Controller
         if (!$blogs) {
             throw $this->createNotFoundException('No article found for id ' . $id);
         }
-        return $this->render('admin/info-article-blog.html.twig', array('blogs' => $blogs));
+        return $this->render('blog/info-article.html.twig', array('blogs' => $blogs));
 
     }
 
